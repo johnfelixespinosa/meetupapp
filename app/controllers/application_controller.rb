245da6d6c1ea7cc147e31855b67110083b62e95a
Johnfelix Espinosa <johnfelix.espinosa@icloud.com>
2019-02-13 18:05:46 -0500

    Initial Commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  before_action :authenticate_user
+
+  private
+
+    def authenticate_user
+      client_id = ENV['MEETUP_CONSUMER_KEY']
+      redirect_uri = CGI.escape("https://localhost:3000/auth")
+      meetup_url = "https://secure.meetup.com/oauth2/authorize?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+      redirect_to meetup_url unless logged_in?
+    end
+
+    def logged_in?
+      !!session[:token]
+    end
+end
+
+# https://api.meetup.com/oauth/request/
+# ?client_id=YOUR_CLIENT_ID
+
+# https://secure.meetup.com/oauth2/authorize
+# ?client_id=YOUR_CONSUMER_KEY
+# &response_type=code
+# &redirect_uri=YOUR_CONSUMER_REDIRECT_URI
